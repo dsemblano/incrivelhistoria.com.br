30b77035e240fb91bf6adb216704580a164500bf
Daniel Semblano <dsemblano@gmail.com>
2023-02-14 06:32:11 -0300

    background colors removed, padding-right from md added

--- site/web/app/themes/IH/resources/views/partials/homeblocks.blade.php
+++ site/web/app/themes/IH/resources/views/partials/homeblocks.blade.php
@@ -1,7 +1,7 @@
 {{-- <div class="flex flex-col md:flex-row  bg-blue-400"> --}}
 <div class="flex flex-col md:flex-row">
     {{-- <x-home-curiosidadeslistas class="mt-4 border-b border-solid border-gray-400 pb-12 lg:w-3/4 bg-yellow-300 lg:pr-4" /> --}}
-    <x-home-curiosidadeslistas class="mt-4 border-b border-solid border-gray-400 pb-12 lg:w-3/4 lg:pr-8" />
+    <x-home-curiosidadeslistas class="mt-4 border-b border-solid border-gray-400 pb-12 lg:w-3/4 md:pr-6 lg:pr-8" />
     {{-- <aside class="sidebar md:w-1/4 bg-green-400"> --}}
     <aside class="sidebar md:w-1/4">
         @include('sections.sidebar')
