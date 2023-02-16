19d7dec43b63ba72961abda18034fc22266167ea
Daniel Semblano <dsemblano@gmail.com>
2023-02-16 14:17:59 -0300

    Lastest posts time included, tags cloud margin top

--- site/web/app/themes/IH/resources/views/partials/lastposts.blade.php
+++ site/web/app/themes/IH/resources/views/partials/lastposts.blade.php
@@ -7,8 +7,13 @@
         <a class="no-underline hover:underline" href="{{ get_permalink() }}">
         {{ the_post_thumbnail('large' ) }}
         <h4 class="text-base lg:text-xl mt-2">{{ the_title() }}</h4>
+        <span>
+            <time class="dt-published" datetime="{{ get_post_time('c', true) }}">
+            {{ get_the_date() }}
+            </time>
+        </span>
         </a>
     </article>
     @endwhile
-    
+
 </section>
