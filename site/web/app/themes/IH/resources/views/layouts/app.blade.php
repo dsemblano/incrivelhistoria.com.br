75bf510eacdc10dd77e1864f920b4344889820a9
Daniel Semblano <dsemblano@gmail.com>
2023-02-17 09:04:55 -0300

    Landscape viewport testing

--- site/web/app/themes/IH/resources/views/layouts/app.blade.php
+++ site/web/app/themes/IH/resources/views/layouts/app.blade.php
@@ -6,14 +6,14 @@
 
   @php $current_page = basename(get_permalink()) @endphp
 
-  <div class="container">
-    <div class="flex flex-col md:flex-row mt-8 justify-between">
-      <main id="main" class="main">
+  <div class="container portrait:bg-yellow-400 landscape:bg-purple-500">
+    <div class="flex flex-col landscape:flex-row md:flex-row mt-8 justify-between">
+      <main id="main" class="main {{ ! is_front_page() ? "landscape:w-3/4" : '' }} landscape:bg-orange-500">
         @yield('content')
       </main>
     
       @if (! is_front_page())
-      <aside class="sidebar md:w-1/4">
+      <aside class="sidebar landscape:w-1/4 lg:w-1/4 landscape:bg-pink-300">
         @include('sections.sidebar')
       </aside>
       @endif
