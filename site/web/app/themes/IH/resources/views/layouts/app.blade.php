0374cdbec21b5dab89dcf6b3e0347c0301c20416
Daniel Semblano <dsemblano@gmail.com>
2023-02-17 10:05:21 -0300

    Landscape viewport all screens

--- site/web/app/themes/IH/resources/views/layouts/app.blade.php
+++ site/web/app/themes/IH/resources/views/layouts/app.blade.php
@@ -6,14 +6,14 @@
 
   @php $current_page = basename(get_permalink()) @endphp
 
-  <div class="container portrait:bg-yellow-400 landscape:bg-purple-500">
-    <div class="flex flex-col landscape:flex-row lg:flex-row mt-8 justify-between">
-      <main id="main" class="main {{ ! is_front_page() ? "landscape:w-3/4" : '' }} landscape:bg-orange-500">
+  <div class="container">
+    <div class="flex flex-col landscape:flex-row lg:flex-row mt-8 justify-between {{ ! is_front_page() ? "gap-4" : '' }}">
+      <main id="main" class="main {{ ! is_front_page() ? "landscape:w-3/4" : '' }}">
         @yield('content')
       </main>
     
       @if (! is_front_page())
-      <aside class="sidebar landscape:w-1/4 lg:w-1/4 landscape:bg-pink-300">
+      <aside class="sidebar landscape:w-1/4 lg:w-1/4">
         @include('sections.sidebar')
       </aside>
       @endif
