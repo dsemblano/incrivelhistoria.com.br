2c42e4ebd7ec49447e204bc21edfc6fb9a8fe0d2
Daniel Semblano <dsemblano@gmail.com>
2023-02-22 16:04:28 -0300

    Footer tinkering

--- site/web/app/themes/IH/resources/views/sections/footer.blade.php
+++ site/web/app/themes/IH/resources/views/sections/footer.blade.php
@@ -1,9 +1,17 @@
 <footer class="content-info bg-orange-400">
   <div class="container bg-purple-500">
-    @if (has_nav_menu('tertiary_navigation'))
-      {!! wp_nav_menu(['theme_location' => 'tertiary_navigation', 'menu_class' => 'sitemap-footer flex flex-col']) !!}
-    @endif
-    {!! get_search_form(false) !!}
-    @include('partials.socialmedia')
+    
+    {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'sitemap-footer flex flex-col py-2 text-black tracking-widest w-full justify-evenly
+          xl:flex-row xl:mt-0 nav text-lg relative']) !!}
+    
+    <div class="iconssocialmedia bg-red-500 flex lg:flex-row justify-around content-around">
+      <div class="w-1/2 md:pr-6 lg:pr-8 bg-green-600">
+        {!! get_search_form(false) !!}
+      </div>
+
+      <div class="w-1/2 bg-green-200">
+        @include('partials.socialmedia')
+      </div>    
+    </div>
   </div>
 </footer>
