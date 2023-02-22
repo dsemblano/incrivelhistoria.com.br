b744357551e67c883c5099fcd38ad2543fe16ce2
Daniel Semblano <dsemblano@gmail.com>
2023-02-22 15:05:07 -0300

    archieve page

--- site/web/app/themes/IH/resources/views/partials/content-page.blade.php
+++ site/web/app/themes/IH/resources/views/partials/content-page.blade.php
@@ -1,3 +1,8 @@
-@php(the_content())
+@if (! is_front_page())
+<article class="prose lg:prose-xl border-b border-solid border-gray-400">
+    @php(the_content())
+</article>
+@endif
+
 
 {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
--- site/web/app/themes/IH/resources/views/partials/content.blade.php
+++ site/web/app/themes/IH/resources/views/partials/content.blade.php
@@ -1,18 +1,21 @@
-<article @php(post_class('prose lg:prose-xl border-b border-solid border-gray-400'))>
+<article @php(post_class(''))>
   <a href="{{ get_permalink() }}">
     {{ the_post_thumbnail('medium_large', array( 'class' => 'w-full' ) ) }}
   </a>
   <header>
-    <h2 class="entry-title">
+    <h2 class="entry-title text-base lg:text-xl mt-2">
       <a href="{{ get_permalink() }}">
         {!! $title !!}
       </a>
     </h2>
 
-    @include('partials.entry-meta')
+    {{-- @include('partials.entry-meta') --}}
+    {{-- <time class="dt-published" datetime="{{ get_post_time('c', true) }}">
+      {{ get_the_date() }}
+    </time> --}}
   </header>
-
+{{-- 
   <div class="entry-summary">
     @php(the_excerpt())
-  </div>
+  </div> --}}
 </article>
