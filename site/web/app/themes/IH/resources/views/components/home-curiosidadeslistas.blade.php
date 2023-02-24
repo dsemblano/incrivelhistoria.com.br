d339beca9292d05eb4a24921a0a9943740b5e195
Daniel Semblano <dsemblano@gmail.com>
2023-02-24 16:19:15 -0300

    Font Awesome + reading time

--- site/web/app/themes/IH/resources/views/components/home-curiosidadeslistas.blade.php
+++ site/web/app/themes/IH/resources/views/components/home-curiosidadeslistas.blade.php
@@ -16,11 +16,7 @@
                 {{ the_title() }}
               </a>
             </h2>
-            <div class="timeread flex flex-row">
-              <i class="fa-regular fa-clock"></i>
-              {!! do_shortcode('[rt_reading_time label="Leitura:" postfix="minutos" postfix_singular="minuto"]') !!}
-              teste
-            </div>
+            @include('partials.readingtime')
         </article>
         @endwhile
         @else
---
+++ site/web/app/themes/IH/resources/views/partials/readingtime.blade.php
@@ -0,0 +1,4 @@
+<div class="timeread flex flex-row">
+    <i class="fa-regular fa-clock mr-1 relative top-0.5"></i>
+    {!! do_shortcode('[rt_reading_time label="Leitura:" postfix="minutos" postfix_singular="minuto"]') !!}
+</div>
