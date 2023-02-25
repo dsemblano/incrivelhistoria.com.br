84151c1b601a8e97be2865b3293618c962ab25aa
Daniel Semblano <dsemblano@gmail.com>
2023-02-24 21:13:08 -0300

    Popular posts without plugin

--- site/web/app/themes/IH/resources/views/sections/sidebar.blade.php
+++ site/web/app/themes/IH/resources/views/sections/sidebar.blade.php
@@ -3,5 +3,38 @@
 </div> --}}
 
 @include('partials.lastposts')
+<h3 class="category-name inline-block text-2xl mb-0 text-red-700">Mais acessadas</h3>
+<?php
+
+// Get popular posts based on post views
+
+$args = array(
+    'post_type' => 'post',
+    'post_status' => 'publish',
+    'posts_per_page' => 5,
+    'meta_key' => 'post_views_count',
+    'orderby' => 'meta_value_num',
+    'order' => 'DESC'
+);
+$query = new WP_Query($args);
+
+if ($query->have_posts()) : ?>
+
+    <ul class="recent-posts mb-6">
+    <?php while ($query->have_posts()) : $query->the_post(); ?>
+        <li class="text-base lg:text-lg mt-2">
+            <a href="<?php the_permalink(); ?>">
+                <?php the_title(); ?>
+            </a>
+            <span class="rt-reading-time block">
+                <?php echo get_post_meta(get_the_ID(), 'post_views_count', true); ?> leituras
+            </span> 
+            
+        </li>
+    <?php endwhile; ?>
+    </ul>
+
+<?php endif; wp_reset_postdata(); ?>
+
 {{-- @include('partials.hrelement') --}}
-@php(dynamic_sidebar('sidebar-primary'))
+@php(dynamic_sidebar('sidebar-primary'))    
