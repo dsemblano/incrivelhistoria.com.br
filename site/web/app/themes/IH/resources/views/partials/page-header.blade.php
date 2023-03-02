b214fc66c52868ceaf1c7ea926ebb2805f9b2001
Daniel Semblano <dsemblano@gmail.com>
2023-03-02 13:37:15 -0300

    Posts title new formatting, mobile fixing

--- site/web/app/themes/IH/resources/views/partials/page-header.blade.php
+++ site/web/app/themes/IH/resources/views/partials/page-header.blade.php
@@ -1,5 +1,6 @@
 @if (! is_front_page())
 <div class="page-header text-red-700">
+  <img src="@asset('images/bg/periodos-bg.webp')" alt="background image categoria" class="" />
   <h1>{!! $title !!}</h1>
 </div>
 @endif
