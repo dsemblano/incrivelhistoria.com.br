ce238e3720b56890392aa7d58c061a9382f2b6da
Daniel Semblano <dsemblano@gmail.com>
2023-03-28 19:08:14 -0300

    All categories page template

---
+++ site/web/app/themes/IH/resources/views/template-allcategories.blade.php
@@ -0,0 +1,14 @@
+{{--
+  Template Name: All categories Template
+--}}
+
+@extends('layouts.app')
+
+@section('content')
+  @while(have_posts()) @php(the_post())
+    @include('partials.page-header')
+    <section id="all-categories" class="md:my-12 md:pt-8">
+        <x-page-allcategories />
+    </section>
+  @endwhile
+@endsection
