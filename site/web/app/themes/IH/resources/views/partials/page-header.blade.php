ba35dc7e98ef62acda0a0e11c66f2556b6d7e8cf
Daniel Semblano <dsemblano@gmail.com>
2023-03-29 11:37:47 -0300

    All categories exclude nossa loja id

--- site/web/app/themes/IH/resources/views/partials/page-header.blade.php
+++ site/web/app/themes/IH/resources/views/partials/page-header.blade.php
@@ -7,7 +7,11 @@
         {!! category_description() !!}
       </p>
     </div>
+    @if (is_page('categorias'))
     <img src="@asset('images/bg/periodos-bg1920.webp')" alt="background image categoria" class="" />
+    @else
+    <img src="{{ get_taxonomy_image(get_queried_object()->term_id) }}" alt="Background imagem da categoria {{get_queried_object()->name}}" style="width: 100%; height: 33rem; object-fit: cover;" class="imagecatbg w-full shadow-xl rounded-t-md wp-post-image">
+    @endif  
   </div>
 
 @endif
