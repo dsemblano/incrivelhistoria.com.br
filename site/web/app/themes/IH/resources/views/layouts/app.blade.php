fa1604d4dc5c7f1a20f1aa4a9b7c1ef4ca685117
Daniel Semblano <dsemblano@gmail.com>
2023-03-29 11:58:13 -0300

    Categories all page with same layout as single category pae

--- site/web/app/themes/IH/resources/views/layouts/app.blade.php
+++ site/web/app/themes/IH/resources/views/layouts/app.blade.php
@@ -6,15 +6,15 @@
 
   @php $current_page = basename(get_permalink()) @endphp
 
-  <div class="{{ ! is_single() && ! is_category() && ! is_tag() ? "container" : "single" }}">
+  <div class="{{ ! is_single() && ! is_category() && ! is_tag() && ! is_page('categorias') ? "container" : "single" }}">
 
-    <div class="{{ ! is_single() && ! is_category() && ! is_tag() ? "flex flex-col landscape:flex-row lg:flex-row mt-8 justify-between lg:gap-4" : "singleclass"}}">
+    <div class="{{ ! is_single() && ! is_category() && ! is_tag() && ! is_page('categorias') ? "flex flex-col landscape:flex-row lg:flex-row mt-8 justify-between lg:gap-4" : "singleclass"}}">
 
-      <main id="main" class="main {{ (! is_front_page() && ! is_single() && ! is_category() && ! is_tag()) ? "landscape:w-3/4" : '' }}">
+      <main id="main" class="main {{ (! is_front_page() && ! is_single() && ! is_category() && ! is_tag() && ! is_page('categorias') ) ? "landscape:w-3/4" : '' }}">
         @yield('content')
       </main>
     
-      @if (! is_front_page() && ! is_single() && ! is_category() && ! is_tag())
+      @if (! is_front_page() && ! is_single() && ! is_category() && ! is_tag() && ! is_page('categorias'))
       <aside class="sidebar landscape:w-1/4 lg:w-1/4 h-">
         @include('sections.sidebar')
       </aside>
--- site/web/app/themes/IH/resources/views/template-allcategories.blade.php
+++ site/web/app/themes/IH/resources/views/template-allcategories.blade.php
@@ -8,7 +8,9 @@
   @while(have_posts()) @php(the_post())
     @include('partials.page-header')
     <section id="all-categories" class="md:my-12 md:pt-8">
+      <div class="container">
         <x-page-allcategories />
+      </div>
     </section>
   @endwhile
 @endsection
