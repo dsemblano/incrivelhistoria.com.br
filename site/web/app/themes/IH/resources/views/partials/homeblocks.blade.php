f5abd4c976f4c14b420e70caa211c85d1d6e9b6b
Daniel Semblano <dsemblano@gmail.com>
2023-04-03 12:15:41 -0300

    footer adjustments mobile css

--- site/web/app/themes/IH/resources/views/partials/homeblocks.blade.php
+++ site/web/app/themes/IH/resources/views/partials/homeblocks.blade.php
@@ -1,7 +1,7 @@
 {{-- <span class="category-name mb-4 text-gray-700 uppercase font-bold">Destaques</span> --}}
-<x-home-curiosidadeslistas id="categorias" class="flex flex-col landscape:flex-row gap-4 lg:flex-row justify-around content-around" />
+<x-home-curiosidadeslistas id="categorias" class="flex flex-col mb-8 landscape:flex-row gap-4 lg:flex-row justify-around content-around" />
 
-@include('partials.hrelement')
+{{-- @include('partials.hrelement') --}}
 
 {{-- <section id="all-categories" class="md:my-12 md:pt-8">
     <x-page-allcategories />
--- site/web/app/themes/IH/resources/views/sections/footer.blade.php
+++ site/web/app/themes/IH/resources/views/sections/footer.blade.php
@@ -1,10 +1,10 @@
 <footer class="content-info bg-ihcor text-white pb-12">
   <div class="container">
     
-    {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'sitemap-footer pt-8 text-white flex flex-col py-2 tracking-widest w-full justify-evenly
-          xl:flex-row xl:mt-0 nav text-lg relative']) !!}
+    {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'border-gray-500 border-b border-solid py-8 sitemap-footer text-white flex flex-col tracking-widest w-full justify-evenly
+          xl:flex-row nav text-lg relative']) !!}
 
-    <div class="flex flex-col md:flex-row justify-between mt-4">
+    <div class="flex flex-col md:flex-row justify-between py-8">
       <div class="formfooter">
         <h3 class="text-2xl">Contato</h3>
         {!! do_shortcode('[contact-form-7 id="18361" title="Contato"]') !!}
@@ -16,7 +16,7 @@
           @include('partials/inputsearch')
         </div>
         
-        <div class="social-icons flex flex-row justify-around text-3xl mt-4">
+        <div class="social-icons flex flex-row justify-around text-3xl mt-8">
           <a href="https://www.instagram.com/incrivelhistoria/" target="_blank"><i class="fa-brands fa-instagram"></i></a>
           <a href="https://www.facebook.com/incrivel.historia" target="_blank"><i class="fa-brands fa-facebook"></i></a>
           <a href="https://www.youtube.com/@incrivelhistoriaoficial/" target="_blank"><i class="fa-brands fa-youtube"></i></a>      
