51a0fd5a45994ad9e9c9687b6e1a0315610be61f
Daniel Semblano <dsemblano@gmail.com>
2023-04-03 11:48:45 -0300

    footer adjustments, left and right

--- site/web/app/themes/IH/resources/scripts/app.js
+++ site/web/app/themes/IH/resources/scripts/app.js
@@ -2,8 +2,9 @@ import domReady from '@roots/sage/client/dom-ready';
 import 'flowbite/dist/flowbite.js';
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
 import { faClock, faCalendarDays } from '@fortawesome/free-regular-svg-icons';
+import { faFacebook, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
 import { faFireFlameCurved } from '@fortawesome/free-solid-svg-icons';
-library.add(faClock, faCalendarDays, faFireFlameCurved);
+library.add(faClock, faCalendarDays, faFireFlameCurved, faFacebook, faInstagram, faYoutube);
 dom.watch();
 
 /**
--- site/web/app/themes/IH/resources/views/sections/footer.blade.php
+++ site/web/app/themes/IH/resources/views/sections/footer.blade.php
@@ -3,23 +3,23 @@
     
     {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'sitemap-footer pt-8 text-white flex flex-col py-2 tracking-widest w-full justify-evenly
           xl:flex-row xl:mt-0 nav text-lg relative']) !!}
-    
-    <div class="iconssocialmedia flex lg:flex-row justify-around content-around">
 
-      <div class="w-1/2">
-        {{-- @include('partials.socialmedia') --}}
-      </div>    
-    </div>
-    <div class="flex flex-row justify-end mt-4">
-      <div class="relative">
-        @include('partials/inputsearch')
+    <div class="flex flex-col md:flex-row justify-between mt-4 bg-red-500">
+      <div class="formfooter container">
+        <h3>Contato</h3>
+        {!! do_shortcode('[contact-form-7 id="18361" title="Contato"]') !!}
       </div>
-    </div>  
-
-    <div class="formfooter">
-      <h3>Contato</h3>  
-      {!! do_shortcode('[contact-form-7 id="18361" title="Contato"]') !!}
+      
+      <div class="footer-right container flex flex-col">
+        @include('partials/inputsearch')
+        <div class="social-icons text-3xl">
+          <a href="https://www.instagram.com/incrivelhistoria/" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+          <a href="https://www.facebook.com/incrivel.historia" target="_blank"><i class="fa-brands fa-facebook"></i></a>
+          <a href="https://www.youtube.com/@incrivelhistoriaoficial/" target="_blank"><i class="fa-brands fa-youtube"></i></a>      
+        </div>
+        
+      </div>
     </div>
-    
+
   </div>
 </footer>
