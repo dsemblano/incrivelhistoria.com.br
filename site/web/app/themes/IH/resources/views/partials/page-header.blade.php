e1d185921ef103484646c89e2dcdf350bd7d5ed4
Daniel Semblano <dsemblano@gmail.com>
2023-04-11 15:58:45 -0300

    Tags page with default background image

--- site/web/app/themes/IH/resources/views/partials/page-header.blade.php
+++ site/web/app/themes/IH/resources/views/partials/page-header.blade.php
@@ -1,4 +1,4 @@
-@if ( is_category() || is_tag() || is_page('categorias') )
+@if ( is_category() || is_page('categorias') || is_tag() )
 
   <div class="page-header">
     <div class="heading-category container relative left-0 top-16 bg-white w-80 md:w-1/3 px-4 pb-4 text-center">
@@ -7,7 +7,7 @@
         {!! category_description() !!}
       </p>
     </div>
-    @if (is_page('categorias'))
+    @if ( is_page('categorias') || is_tag() )
     <img src="@asset('images/bg/periodos-bg1920.webp')" alt="background image categoria" class="" />
     @else
     <img src="{{ get_taxonomy_image(get_queried_object()->term_id) }}" alt="Background imagem categoria {{get_queried_object()->name}}" style="width: 100%; height: 33rem; object-fit: cover;" class="imagecatbg w-full shadow-xl rounded-t-md wp-post-image">
