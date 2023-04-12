31b8c089c7ca1165cbe98bdca130da5c83ceebc5
Daniel Semblano <dsemblano@gmail.com>
2023-04-11 23:30:08 -0300

    Google Tag Manager code fix

--- site/web/app/themes/IH/index.php
+++ site/web/app/themes/IH/index.php
@@ -10,12 +10,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
-    <?php echo \Roots\view('layouts/head/gtaghead')->render(); ?>
+    <?php echo \Roots\view('partials/gtaghead')->render(); ?>
   </head>
 
   <body <?php (WP_ENV !== 'production') ? body_class('debug-screens') : body_class(''); ?>> 
     <?php wp_body_open(); ?>
-    <?php echo \Roots\view('partials/snippets/gtagbody')->render(); ?>
+    <?php echo \Roots\view('partials/gtagbody')->render(); ?>
     <?php do_action('get_header'); ?>
 
     <div id="app">
