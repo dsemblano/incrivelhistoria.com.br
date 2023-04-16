3dd83756c2c00e054b585d3145d08656fe1b64b1
Daniel Semblano <dsemblano@gmail.com>
2023-04-15 21:15:54 -0300

    Destaques new layout fixes

--- site/web/app/themes/IH/resources/views/components/home-destaques.blade.php
+++ site/web/app/themes/IH/resources/views/components/home-destaques.blade.php
@@ -41,12 +41,32 @@
           @else
           {{-- segundo loop --}}
             @if ($second_loop == false)
-            <div class="flex flex-col lg:flex-row segundoloop">
+            <div class="flex flex-col lg:flex-row segundoloop mt-4">
             @endif
           @php $second_loop = true; @endphp
           @if ($second_loop) 
-            <article class="rounded bg-gray-70 hover:bg-gray-100 hover:rounded mb-2">
-                <div class="p-4 flex flex-row gap-2">
+            <article class="rounded bg-gray-70 hover:bg-gray-100 hover:rounded mb-6">
+                {{-- inicio mobile --}}
+                <div class="flex flex-row justify-between gap-x-4 md:hidden">
+                    <a class="" href="{{ get_permalink() }}">
+                        <figure class="imgpost">
+                            {{ the_post_thumbnail('curiosidades_small', array( 'class' => 'w-full shadow-xl rounded-t-md' ) ) }}
+                        </figure>
+                    </a>
+                    <h2 class="text-lg md:text-base lg:text-lg font-bold">
+                        <a class="postslinks" href="{{ get_permalink() }}">
+                            {{ the_title() }}
+                        </a>
+                    </h2>
+                </div>
+                <p class="block md:hidden mt-2 excerpt">
+                    <a class="postslinks" href="{{ get_permalink() }}">
+                      {{ get_the_excerpt() }}
+                    </a>
+                  </p>
+                {{-- fim mobile   --}}
+
+                <div class="hidden md:flex p-4 flex-row gap-2">
                   <a class="w-1/2 md:w-full lg:w-1/2" href="{{ get_permalink() }}">
                     <figure class="imgpost">
                       {{ the_post_thumbnail('curiosidades_small', array( 'class' => 'w-full shadow-xl rounded-t-md' ) ) }}
