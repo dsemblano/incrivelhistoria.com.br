0321239c3bd732009ab499f93fa9127957619f14
Daniel Semblano <dsemblano@gmail.com>
2023-04-17 19:12:43 -0300

    Trellis provision production for new name theme

--- site/web/app/themes/IH/resources/views/partials/hrelement.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="flex items-center justify-center my-4">
-    <div class="flex-1 h-px bg-gray-300"></div>
-    <div class="w-3 h-3 mx-4 rounded-full bg-gray-300"></div>
-    <div class="flex-1 h-px bg-gray-300"></div>
-</div>
--- site/web/app/themes/IH/resources/views/partials/threedots.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="flex space-x-8 items-center justify-center m-6 lg:m-14">
-    <div class="w-1 h-1 rounded-full bg-gray-600"></div>
-    <div class="w-1 h-1 rounded-full bg-gray-600"></div>
-    <div class="w-1 h-1 rounded-full bg-gray-600"></div>
-  </div>
