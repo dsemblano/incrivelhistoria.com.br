02bcf6ed9562173c97f7d54485b6cfcabe0c23ec
Daniel Semblano <dsemblano@gmail.com>
2023-04-17 22:51:15 -0300

    Author box fixed

--- site/web/app/themes/incrivelhistoria/app/filters.php
+++ site/web/app/themes/incrivelhistoria/app/filters.php
@@ -26,37 +26,23 @@ add_filter('comment_form_default_fields', function ($fields) {
 });
 
 // Author box
-    // add_filter('the_content', function($content) {
-    //     global $post;
-    
-    //     // Check if the post has an author
-    //     if (get_the_author_meta('ID')) {
-    //         // Get the author's display name and bio
-    //         $author_id = get_the_author_meta('ID');
-    //         $author_name = get_the_author_meta('display_name');
-    //         $author_bio = get_the_author_meta('description');
-    
-    //         // Check if the current user is the author
-    //         $current_user_id = get_current_user_id();
-    //         if ($current_user_id != $author_id) {
-    //             // Build the author box HTML
-    //             $author_box = '<div class="flex flex-col items-center author author-box mt-6">';
-    //             $author_box .= '<figure class="!m-0">';
-    //             $author_box .= '<figcaption class="flex items-center !mt-0">';
-    //             $author_box .= '<div class="author-avatar avatar avatar-128 photo rounded-full">' . get_avatar( get_the_author_meta('ID'), 128, '', 'avatar', array('class' => 'rounded-full !m-0') ) . '</div>';
-    //             $author_box .= '</figure>';
-    //             $author_box .= '<span class="p-author author-info text-center mt-8">';
-    //             $author_box .= '<span class="font-semibold p-author mt-4">' . $author_name . '</span>';
-    //             $author_box .= '<p class="author-bio">' . $author_bio . '</p>';
-    //             $author_box .= '</span>';
-    //             $author_box .= '</div>';
-    
-    //             // Add the author box to the post content
-    //             $content .= $author_box;
-    //         }
-    //     }
-    //     if (! is_page() ) {
-    //         return $content;
-    //     }
-        
-    // });
+
+add_filter('the_content', function($content) {
+    if (is_single()) {
+        $author_name = get_the_author_meta('display_name');
+        $author_bio = get_the_author_meta('description');
+
+        $author_box = '<div class="flex flex-col items-center author author-box mt-6">';
+        $author_box .= '<figure class="!m-0">';
+        $author_box .= '<figcaption class="flex items-center !mt-0">';
+        $author_box .= '<div class="author-avatar avatar avatar-128 photo rounded-full">' . get_avatar( get_the_author_meta('ID'), 128, '', 'avatar', array('class' => 'rounded-full !m-0') ) . '</div>';
+        $author_box .= '</figure>';
+        $author_box .= '<span class="p-author author-info text-center mt-8">';
+        $author_box .= '<span class="font-semibold p-author mt-4">' . $author_name . '</span>';
+        $author_box .= '<p class="author-bio">' . $author_bio . '</p>';
+        $author_box .= '</span>';
+        $author_box .= '</div>';
+        $content .= $author_box;
+    }
+    return $content;
+});
