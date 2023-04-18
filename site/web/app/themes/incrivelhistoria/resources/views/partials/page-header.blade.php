3aeda10d381e6d662759433ca76ddf01e5a181e8
Daniel Semblano <dsemblano@gmail.com>
2023-04-18 03:53:34 -0300

    category title from pages top adjustmen

--- site/web/app/themes/incrivelhistoria/resources/views/partials/page-header.blade.php
+++ site/web/app/themes/incrivelhistoria/resources/views/partials/page-header.blade.php
@@ -1,7 +1,7 @@
 @if ( is_category() || is_page('categorias') || is_tag() )
 
   <div class="page-header">
-    <div class="heading-category container relative left-0 top-16 bg-white w-80 md:w-1/3 px-4 pb-4 text-center">
+    <div class="heading-category container relative left-0 top-10 lg:top-16 bg-white w-80 md:w-1/3 px-4 pb-4 text-center">
       <h1 class="text-ihcat font-bold text-4xl lg:text-6xl">{!! $title !!}</h1>
       <p class="category-description lg:text-xl my-4">
         {!! category_description() !!}
