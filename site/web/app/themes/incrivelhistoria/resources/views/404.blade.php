197947480c80bb93c4e46f7f9b64674bd6606278
Daniel Semblano <dsemblano@gmail.com>
2023-04-17 21:27:25 -0300

    Lato font for excerpt, 404 page translation

--- site/web/app/themes/incrivelhistoria/resources/views/404.blade.php
+++ site/web/app/themes/incrivelhistoria/resources/views/404.blade.php
@@ -5,7 +5,7 @@
 
   @if (! have_posts())
     <x-alert type="warning">
-      {!! __('Sorry, but the page you are trying to view does not exist.', 'sage') !!}
+      {!! __('Página não encontrada!.', 'sage') !!}
     </x-alert>
 
     <div class="flex flex-row justify-start mt-4">
