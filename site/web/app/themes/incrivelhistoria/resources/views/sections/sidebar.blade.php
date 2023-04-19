5ef0ec32fdb24838f6bd3d2dafe8b43fea018848
Daniel Semblano <dsemblano@gmail.com>
2023-04-19 16:43:05 -0300

    wordpress-popular-posts plugin + shortcode

--- site/web/app/themes/incrivelhistoria/resources/views/sections/sidebar.blade.php
+++ site/web/app/themes/incrivelhistoria/resources/views/sections/sidebar.blade.php
@@ -12,42 +12,10 @@
     
 </h3>
 
-<?php
-// Get popular posts based on post views
-$args = array(
-    'post_type' => 'post',
-    'post_status' => 'publish',
-    'posts_per_page' => 5,
-    'meta_key' => 'post_views_count',
-    'orderby' => 'meta_value_num',
-    'order' => 'DESC'
-);
-$query = new WP_Query($args);
-
-if ($query->have_posts()) : ?>
-
     <section id="popular-posts" class="mb-6">
-    <?php while ($query->have_posts()) : $query->the_post(); ?>
-        <article class="text-base mt-2 flex flex-row gap-x-4">
-            <a href="{{ get_permalink() }}">
-                <figure class="imgpost">
-                    {{ the_post_thumbnail('thumbnail', array( 'class' => 'max-w-none' ) ) }}
-                </figure>    
-            </a>
-            <div class="title-reading flex flex-col">
-                <a href="<?php the_permalink(); ?>">
-                    <?php the_title(); ?>
-                </a>
-                <span class="rt-reading-time block">
-                    <?php echo get_post_meta(get_the_ID(), 'post_views_count', true); ?> leituras
-                </span>
-            </div>
-            
-        </article>
-    <?php endwhile; ?>
+    {!! do_shortcode('[wpp]') !!}
     </section>
 
-<?php endif; wp_reset_postdata(); ?>
 @include('partials.hrelement')
 @include('partials.lastposts')
 @include('partials.hrelement')
