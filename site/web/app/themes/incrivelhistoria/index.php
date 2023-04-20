aa9c33ca65625a8f127851fc93f1e619c9e3f57b
Daniel Semblano <dsemblano@gmail.com>
2023-04-20 19:13:53 -0300

    deactivated gtag for a while

--- site/web/app/themes/incrivelhistoria/index.php
+++ site/web/app/themes/incrivelhistoria/index.php
@@ -4,18 +4,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
-    <?php if (WP_ENV == 'production') : ?>
-    <?php echo \Roots\view('partials/gtaghead')->render(); ?>
-    <?php endif ?>
+
     <?php echo \Roots\view('partials/favicon')->render(); ?>
 
   </head>
 
   <body> 
     <?php wp_body_open(); ?>
-    <?php if (WP_ENV == 'production') : ?>
-    <?php echo \Roots\view('partials/gtagbody')->render(); ?>
-    <?php endif ?>
+
     <?php do_action('get_header'); ?>
 
     <div id="app">
