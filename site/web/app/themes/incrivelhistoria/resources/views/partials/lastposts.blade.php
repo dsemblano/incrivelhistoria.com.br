068b73a8a8e52eb564f48c6638672d04daf38878
Daniel Semblano <dsemblano@gmail.com>
2023-04-24 02:04:45 -0300

    Semantic sidebar with ol list and articles inside

--- site/web/app/themes/incrivelhistoria/resources/views/partials/lastposts.blade.php
+++ site/web/app/themes/incrivelhistoria/resources/views/partials/lastposts.blade.php
@@ -5,22 +5,27 @@
         </strong>        
     </h3>
     @php $lastposts = lastposts(4); @endphp
-    @while ($lastposts->have_posts()) @php $lastposts->the_post() @endphp
-    <article class="text-base mt-2 flex flex-row gap-x-4">
-        <a href="{{ get_permalink() }}">
-            <figure class="imgpost">
-                {{ the_post_thumbnail('thumbnail', array( 'class' => 'max-w-none' ) ) }}
-            </figure>    
-        </a>
-        <div class="title-reading flex flex-col">
-            <a href="<?php the_permalink(); ?>">
-                <?php the_title(); ?>
-            </a>
-            <time class="dt-published text-sm inline-block mt-" datetime="{{ get_post_time('c', true) }}">
-                {{ get_the_date() }}
-            </time>
-        </div>
-        
-    </article>
+    <ol id="lastposts-list">
+        @while ($lastposts->have_posts()) @php $lastposts->the_post() @endphp
+        <li>
+            <article class="text-base mt-2 flex flex-row gap-x-4">
+                <a href="{{ get_permalink() }}">
+                    <figure class="imgpost">
+                        {{ the_post_thumbnail('thumbnail', array( 'class' => 'max-w-none' ) ) }}
+                    </figure>    
+                </a>
+                <div class="title-reading flex flex-col">
+                    <a href="<?php the_permalink(); ?>">
+                        <?php the_title(); ?>
+                    </a>
+                    <time class="dt-published text-sm inline-block mt-" datetime="{{ get_post_time('c', true) }}">
+                        {{ get_the_date() }}
+                    </time>
+                </div>
+                
+            </article>
+        </li>
     @endwhile
+    </ol>
+    
 </section>
