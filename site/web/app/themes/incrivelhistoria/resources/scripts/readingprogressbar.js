4192fff388b594f45c3c6f0f4c371b66c129a367
Daniel Semblano <dsemblano@gmail.com>
2023-04-24 08:19:27 -0300

    Reading progress bar update accuracy

--- site/web/app/themes/incrivelhistoria/resources/scripts/readingprogressbar.js
+++ site/web/app/themes/incrivelhistoria/resources/scripts/readingprogressbar.js
@@ -1,5 +1,5 @@
-// Get the height of the content area
-var contentHeight = document.body.clientHeight;
+// Get the height of the article
+var articleHeight = document.querySelector('.type-post').clientHeight;
 
 // Update the progress bar as the user scrolls
 window.addEventListener("scroll", function() {
@@ -8,14 +8,14 @@ window.addEventListener("scroll", function() {
   var windowHeight = window.innerHeight;
 
   // Calculate the progress percentage
-  var progress = Math.min(scrollTop / (contentHeight - windowHeight), 1);
+  var progress = Math.min(scrollTop / (articleHeight - windowHeight), 1);
 
   // Update the progress bar width
   var progressBar = document.querySelector(".reading-progress");
   progressBar.style.width = progress * 100 + "%";
 
   // Check if the user has reached the end of the content
-  if (scrollTop + windowHeight === contentHeight) {
+  if (scrollTop + windowHeight === articleHeight) {
     progressBar.style.width = "100%";
   }
 });
