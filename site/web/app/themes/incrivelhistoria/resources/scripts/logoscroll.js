8655a54d8aea42f374617bf38a314f0e5b6af8f2
Daniel Semblano <dsemblano@gmail.com>
2024-07-16 17:18:23 -0300

    Deleted some unused plugins, arrow top back

--- site/web/app/themes/incrivelhistoria/resources/scripts/logoscroll.js
+++ site/web/app/themes/incrivelhistoria/resources/scripts/logoscroll.js
@@ -1,18 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
     //hide or show the "back to top" link
     window.onscroll = function() {
+        if (window.pageYOffset > 300) {
+            document.querySelector('.cd-top').classList.add('cd-is-visible');
+            document.querySelector('.cd-top').classList.remove('cd-fade-out');
+        } else {
+            document.querySelector('.cd-top').classList.remove('cd-is-visible');
+        }
+        if (window.pageYOffset > 1200) {
+            document.querySelector('.cd-top').classList.add('cd-fade-out');
+        }
         if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-            
-            document.getElementById("banner-nav").classList.add('shrink', 'h-16');
-            document.getElementById("menu-pages").classList.remove('hidden', 'xl:block');
-            document.getElementById("menu-pages").classList.add('hidden');
-            document.getElementById("Layer_1").classList.add('w-24');
+            document.getElementById("logo").classList.add("shrink", "bottom-4");
+            document.getElementById("logosurname").classList.add("hidden");
+            document.getElementById("logosurnamepage").classList.remove("hidden");
+            document.getElementById("logosurnamepage").classList.add("block");
             
         } else {
-            document.getElementById("banner-nav").classList.remove('shrink', 'h-16');
-            document.getElementById("menu-pages").classList.remove('hidden');
-            document.getElementById("menu-pages").classList.add('hidden', 'xl:block');
-            document.getElementById("Layer_1").classList.remove('w-24');
+            document.getElementById("logo").classList.remove("shrink");
+            document.getElementById("logosurname").classList.remove("hidden");
+            document.getElementById("logosurnamepage").classList.remove("block");
+            document.getElementById("logosurnamepage").classList.add("hidden");
         }
     };
 });
