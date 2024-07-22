45a41eec8c5d052407857e71aa73caaba76fd26c
Daniel Semblano <dsemblano@gmail.com>
2024-07-22 11:32:00 -0300

    read time svg icon

---
+++ site/web/app/themes/incrivelhistoria/resources/views/partials/svgtime.blade.php
@@ -0,0 +1,17 @@
+{{-- <svg class="inline-block" fill="#475569" height="1rem" width="1rem" version="1.1" id="Layer_1"
+    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512"
+    xml:space="preserve">
+    <g>
+        <g>
+            <path d="M256,0C114.842,0,0,114.842,0,256s114.842,256,256,256s256-114.842,256-256S397.158,0,256,0z M374.821,283.546H256
+        c-15.148,0-27.429-12.283-27.429-27.429V137.295c0-15.148,12.281-27.429,27.429-27.429s27.429,12.281,27.429,27.429v91.394h91.392
+        c15.148,0,27.429,12.279,27.429,27.429C402.249,271.263,389.968,283.546,374.821,283.546z" />
+        </g>
+    </g>
+</svg> --}}
+
+<svg class="inline-block" width="1rem" height="1rem" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+        d="M12 7V12L14.5 10.5M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
+        stroke="#BB2649" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+</svg>
