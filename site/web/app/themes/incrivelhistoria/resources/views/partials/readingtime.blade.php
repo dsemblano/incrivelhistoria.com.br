f125fe00dc1baedf158bb8cb6952fdad163532c1
Daniel Semblano <dsemblano@gmail.com>
2024-07-22 11:47:21 -0300

    read time font size

--- site/web/app/themes/incrivelhistoria/resources/views/partials/readingtime.blade.php
+++ site/web/app/themes/incrivelhistoria/resources/views/partials/readingtime.blade.php
@@ -1,5 +1,5 @@
-<div class="timeread flex flex-row text-xl">
-    <div class="text-lg text-ihcat">
+<div class="timeread flex flex-row">
+    <div class="text-ihcat text-sm">
         @include('partials/svgtime')
         {!! do_shortcode('[rt_reading_time postfix="minutos" postfix_singular="minuto"]') !!}
     </div>
