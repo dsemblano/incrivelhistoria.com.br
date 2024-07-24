d3646e48dddc53864fb7c4d54f4b008a33ec6aa2
Daniel Semblano <dsemblano@gmail.com>
2024-07-24 20:25:16 -0300

    Container pages leghth fixed

--- site/web/app/themes/incrivelhistoria/resources/views/page.blade.php
+++ site/web/app/themes/incrivelhistoria/resources/views/page.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 @while(have_posts()) @php(the_post())
-<section class="gap-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mt-5 w-full lg:pr-8 mb-6">
+<section id="{{ basename(get_permalink()) }}-page" class="pages">
   @include('partials.page-header')
   @includeFirst(['partials.content-page', 'partials.content'])
 </section>
