e6e49247c07c682c9c2d258573cb47d6c16d08a6
Daniel Semblano <dsemblano@gmail.com>
2025-02-17 17:08:26 -0300

    Installed fontfaceobserver and configured. Testing.

--- site/web/app/themes/incrivelhistoria/resources/scripts/app.js
+++ site/web/app/themes/incrivelhistoria/resources/scripts/app.js
@@ -2,6 +2,37 @@ import domReady from '@roots/sage/client/dom-ready';
 import './arrowtop.js';
 import 'flowbite/dist/flowbite.js';
 import './logoscroll.js';
+import FontFaceObserver from 'fontfaceobserver';
+
+
+// Create an instance for Open Sans (variable font)
+const openSans = new FontFaceObserver('Open Sans', {
+  weight: 400,
+  style: 'normal',
+});
+
+// Create an instance for Lato Bold (non-variable font)
+const latoBold = new FontFaceObserver('Lato Bold', {
+  weight: 900,
+  style: 'normal',
+});
+
+const lato = new FontFaceObserver('Lato', {
+  style: 'normal',
+});
+
+const jenson = new FontFaceObserver('Jenson', {
+  style: 'normal',
+});
+
+// Use Promise.all to wait for all fonts to load
+Promise.all([openSans.load(), latoBold.load(), lato.load(), jenson.load()])
+  .then(() => {
+    console.log('All fonts have loaded.');
+  })
+  .catch((error) => {
+    console.log('One or more fonts failed to load:', error);
+  });
 
 // import { partytownSnippet } from '@builder.io/partytown/integration';
 // const snippetText = partytownSnippet();
