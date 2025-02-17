dfb9c30f852573da6ec32924f7564b5439506ec1
Daniel Semblano <dsemblano@gmail.com>
2025-02-16 22:18:25 -0300

    Minor changes author and header single post style

--- site/web/app/themes/incrivelhistoria/resources/views/partials/entry-meta.blade.php
+++ site/web/app/themes/incrivelhistoria/resources/views/partials/entry-meta.blade.php
@@ -4,7 +4,7 @@
       {{-- {!! get_avatar( get_the_author_meta('ID'), 64, '', 'avatar', array('class' => 'rounded-full !m-0') ); !!} --}}
       <img src="@asset('images/Eudes-Bezerra.webp')" alt="Autor Eudes Bezerra" class="rounded-full" />
   </figure>
-  <div class="text-ihcat p-author h-card lg:w-4/5">
+  <div class="text-ihcat p-author h-card lg:w-4/5 lg:ml-8">
     <div class="inline-block text-center">
       {{ __('Autor: ', 'sage') }}<span class="font-bold">{{ get_the_author() }}</span>
       <p class="author-bio text-ihcinza text-sm text-center">
