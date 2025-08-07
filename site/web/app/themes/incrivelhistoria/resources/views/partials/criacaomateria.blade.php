ccf02adefc9f0bc34036067a9c8ccfe7f3277050
Daniel Semblano <dsemblano@gmail.com>
2025-08-07 11:32:24 -0300

    composer update plugins and WP

--- site/web/app/themes/incrivelhistoria/resources/views/partials/criacaomateria.blade.php
+++ site/web/app/themes/incrivelhistoria/resources/views/partials/criacaomateria.blade.php
@@ -1,10 +1,8 @@
 <div class="text-base leading-6 text-gray-600">
     <time class="dt-published text-sm" datetime="{{ get_post_time('c', true) }}">
+      <span>Postado em {{ the_time('j F Y') }}<br></span>
       @if ( get_the_modified_time( 'U' ) > get_the_time( 'U' ) )
-      <span>Matéria criada em {{ the_time('j F Y') }}<br></span>
-      <span>Atualizada em {{ the_modified_time('j F Y') }}</span>
-      @else
-      <span>Matéria criada em {{ the_time('j F Y') }} </span>
+        <span>Atualizado em {{ the_modified_time('j F Y') }}</span>
       @endif
     </time>
   </div>
